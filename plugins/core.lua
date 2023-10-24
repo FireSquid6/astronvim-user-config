c2412dca4ca824acaaab918e2f7a373bf8dc7ba4
firesquid <jdeiss06@gmail.com>
2023-10-24 18:57:47 -0500

    fix postspace

--- plugins/core.lua
+++ plugins/core.lua
@@ -23,7 +23,7 @@ return {
   {
     "nvim-neo-tree/neo-tree.nvim",
     opts = function(_, opts)
-      opts.filesystem.filtered_items = { 
+      opts.filesystem.filtered_items = {
         hide_dotfiles = false,
         hide_gitignored = false,
       }
