cf0d82bfe9eb126aead136849960a4c1ac1d7954
firesquid <jdeiss06@gmail.com>
2023-11-16 18:19:48 -0600

    add neorg

--- plugins/user.lua
+++ plugins/user.lua
@@ -69,5 +69,29 @@ return {
       })
     end
   },
+  {
+    "nvim-neorg/neorg",
+    lazy = false,
+    after = "nvim-treesitter",
+    build = ":Neorg sync-parsers",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup {
+        load = {
+          ["core.defaults"] = {}, -- Loads default behaviour
+          ["core.concealer"] = {}, -- Adds pretty icons to your documents
+          -- ["core.esupports.hop"] = {},
+          ["core.dirman"] = { -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/notes",
+              },
+              default_workspace = "notes",
+            },
+          },
+        },
+      }
+    end,
+  },
 }
 
